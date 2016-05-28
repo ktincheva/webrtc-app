d481f019673a8948811967ef79bd45b422b80839
Kameliya Tincheva <ktincheva@gmail.com>
2016-05-28 19:35:40 +0300

    Some modification and refactoring

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -79,8 +79,8 @@ var chatApp = angular.module('publicApp', [
             SIGNALIG_SERVER_URL: 'https://10.2.2.201:5555',
            //SIGNALIG_SERVER_URL: 'https://192.168.1.7:5555',
            max_connections: 5,
-           apiUrl: "https://www.b4dating.lan/chatApi/",
-           siteUrl: "https://www.b4dating.lan",
+           apiUrl: "https://10.2.2.201/chatApi/",
+           siteUrl: "https://0.2.2.201",
 
         })
         .run(function () {
